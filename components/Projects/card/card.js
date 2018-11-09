aada6f0d7e86e304740e102848e66d0da931ec64
rajjix <rajjix.dev.1992@hotmail.com>
2018-11-09 08:26:11 +0200

    card

---
+++ components/Projects/card/card.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Card, Grid, Button, CardActionArea, CardMedia, CardContent, Typography, CardActions } from '@material-ui/core';
+
+export default (props) => {
+  return(
+    <Card id="reactpro-card">
+      <CardActionArea>
+        <CardMedia
+          image={ props.image }
+          title={ props.imageTitle }
+          style= {{ height: props.imageHeight }}
+        />
+        <CardContent style={{textAlign:"center"}}>
+          <Typography gutterBottom variant="h5" component="h2">
+            { props.children }
+          </Typography>
+          <Typography component="p" id="description" style={{textAlign:"justify"}}>
+            { props.description }
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Grid container style={{textAlign: "center"}}>
+          <Grid item xs={12} md={6}>
+            <Button href={ props.github } target="_blank" id="card-button-react" disabled={props.disabled}>
+              Github
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Button href={ props.live } id="card-button-react" disabled={props.disabled}>
+              LiveDemo
+            </Button>
+          </Grid>
+        </Grid>
+      </CardActions>
+    </Card>
+  );
+}
