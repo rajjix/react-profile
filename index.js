30f0c4be03deefba6327dc36dde764aed5c783d5
rajjix <rajjix.dev.1992@hotmail.com>
2018-11-23 17:31:53 +0000

    👨 Profile v1.0

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+
 ReactDOM.render(
       <App /> 
   , document.getElementById('root')
 );
+
+serviceWorker.register();
