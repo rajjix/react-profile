586c810e40c6edd98ed555fad7aa59c3c6a88cce
rajjix <rajjix.dev.1992@hotmail.com>
2018-11-23 17:19:33 +0000

    Updates

---
+++ precache-manifest.afe477d00dc87e1ffa2d8ba1a14a8917.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f23c5d7dd0750185bc78",
+    "url": "/react-profile/static/css/main.ff4d2862.chunk.css"
+  },
+  {
+    "revision": "f23c5d7dd0750185bc78",
+    "url": "/react-profile/static/js/main.f23c5d7d.chunk.js"
+  },
+  {
+    "revision": "7fd4ea4f5194229aca80",
+    "url": "/react-profile/static/js/1.7fd4ea4f.chunk.js"
+  },
+  {
+    "revision": "9b36471a9873008175f1",
+    "url": "/react-profile/static/css/2.ca70cdd4.chunk.css"
+  },
+  {
+    "revision": "9b36471a9873008175f1",
+    "url": "/react-profile/static/js/2.9b36471a.chunk.js"
+  },
+  {
+    "revision": "e12af1d01f177a178344",
+    "url": "/react-profile/static/css/3.036707d9.chunk.css"
+  },
+  {
+    "revision": "e12af1d01f177a178344",
+    "url": "/react-profile/static/js/3.e12af1d0.chunk.js"
+  },
+  {
+    "revision": "27db701ed49aa20d2527",
+    "url": "/react-profile/static/js/runtime~main.27db701e.js"
+  },
+  {
+    "revision": "4fcd870a50c21775d2208d0936d602db",
+    "url": "/react-profile/index.html"
+  }
+];
